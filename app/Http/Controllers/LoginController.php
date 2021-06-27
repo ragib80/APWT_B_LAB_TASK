ba5d7aa7647a63c2ab4ab000369a562a41d8deda
ragib80 <72729030+ragib80@users.noreply.github.com>
2021-06-28 02:12:51 +0600

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,9 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\http\Requests\loginRequest;
 use App\Models\Admin;
-use App\Models\Customer;
 use App\Models\User;
-use App\Models\Accountant;
+
 
 class LoginController extends Controller
 {
@@ -34,55 +33,6 @@ class LoginController extends Controller
 
 
 
-           /* if($admin->password == $req->password){
-                $req->session()->put('user_name',$admin->user_name);
-                $req->session()->put('user_type',$admin->user_type);
-                return redirect('home');
-            }
-            else{
-                $req->session()->flash('err',"wrong password");
-                return redirect()->back();
-            }
-        }
-        elseif($ad = Customer::where('email',$req->email)->first()){
-            if($ad->password == $req->password){
-                $req->session()->put('user_name',$ad->user_name);
-                $req->session()->put('user_type',$ad->user_type);
-                return redirect('home');
-            }
-            else{
-                $req->session()->flash('err',"wrong password");
-                return redirect()->back();
-            }
-        }
-        elseif($ad = Vendor::where('email',$req->email)->first()){
-            if($ad->password == $req->password){
-                $req->session()->put('user_name',$ad->user_name);
-                $req->session()->put('user_type',$ad->user_type);
-                return redirect('home');
-            }
-            else{
-                $req->session()->flash('err',"wrong password");
-                return redirect()->back();
-            }
-        }
-        elseif($ad = Accountant::where('email',$req->email)->first()){
-            if($ad->password == $req->password){
-                $req->session()->put('user_name',$ad->user_name);
-                $req->session()->put('user_type',$ad->user_type);
-                return redirect('home');
-            }
-            else{
-                $req->session()->flash('err',"wrong password");
-                return redirect()->back();
-            }
-        }
-        else{
-            $req->session()->flash('err',"invalid user");
-            return redirect()->back();
-        }
-
-        // return redirect('login');
-    }*/
+           
 }
     }
